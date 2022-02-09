5eefd7cc56c75ca7f0545cd64d9da011f75a3fdd
Brian Angulo <bangulo219@gmail.com>
2022-02-09 00:42:39 -0600

    feat: register controller with saving logic

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create(Request $request) {
+        $request->validate([
+            'username' => 'required|unique:users|max:255',
+            'score' => 'required|max:255',
+        ]);
+        $user = new User();
+        $user->username = $request->username;
+        $user->score = $request->score;
+        $user->save();
+    }
+}
