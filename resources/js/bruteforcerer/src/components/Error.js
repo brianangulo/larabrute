b30539f5687d1707cacae2ea8cd78843b9710a7b
Brian Angulo <bangulo219@gmail.com>
2022-02-12 22:23:25 -0600

    style: cleaning up code a bit

--- resources/js/bruteforcerer/src/components/Error.js
+++ resources/js/bruteforcerer/src/components/Error.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Error(props) {
-    
+
     return (
-        <p hidden={props.isHidden} className="row text-center justify-content-center" style={{color: "red", fontWeight: "bold"}} >For the sake of your time we can only test 4 alpha-num characters or less!</p>
+        <p hidden={props.isHidden}
+            className="row text-center justify-content-center"
+            style={{ color: "red", fontWeight: "bold" }} >
+            For the sake of your time we can only test 4 alpha-num characters or less!
+        </p>
     );
 }
 
