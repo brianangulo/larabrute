8999f543ab00a28fe851abc02629867bae063757
Brian Angulo <bangulo219@gmail.com>
2022-04-08 00:48:10 -0500

    feat: adding isEmpty check

--- resources/js/bruteforcerer/src/components/Register.js
+++ resources/js/bruteforcerer/src/components/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, } from 'react';
 import { useRegisterUsername } from '../hooks';
 import { Button, Modal, Form } from 'react-bootstrap';
 
@@ -7,29 +7,41 @@ function RegisterModal() {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [username, setUsername] = useState('');
+  const [isEmpty, setIsEmpty] = useState(false);
 
   const handleClose = useCallback(() => {
     setUsername('');
     setError(false);
+    setIsEmpty(false);
     setShow(false);
   }, []);
+
   const handleShow = () => setShow(true);
 
-  const handleRegister = useCallback(
-    async (username) => {
+  const handleRegister = useCallback(async (username) => {
+    const isUserNameEmpty = username.length === 0;
+    if (isUserNameEmpty) {
+      setIsEmpty(true);
+      if (error) {
+        setError(false);
+      }
+    }
+    if (!isUserNameEmpty) {
       setLoading(true);
       // eslint-disable-next-line react-hooks/rules-of-hooks
       const registerResponse = await useRegisterUsername(username);
       if (!registerResponse) {
         setError(true);
+        if (isEmpty) {
+          setIsEmpty(false);
+        }
       }
       setLoading(false);
       if (registerResponse) {
         setShow(false);
       }
-    },
-    []
-  );
+    }
+  }, [error, isEmpty]);
 
   return (
     <>
@@ -53,6 +65,11 @@ function RegisterModal() {
               Username is taken or error occurred. Please try again.
             </p>
           )}
+          {isEmpty && (
+            <p className="text-danger" style={{ fontSize: 'medium' }}>
+              Username is empty. Please try again.
+            </p>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -61,7 +78,7 @@ function RegisterModal() {
           <Button
             disabled={loading}
             variant="primary"
-            onClick={() => handleRegister(username)}
+            onClick={() => handleRegister(String(username))}
           >
             {loading ? '...loading' : 'Submit'}
           </Button>
