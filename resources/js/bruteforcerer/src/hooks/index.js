ce561253617ae9c2eb8d5feaaf565dbb93d01b16
Brian Angulo <bangulo219@gmail.com>
2022-04-10 23:59:17 -0500

    feat: removing register logic code

--- resources/js/bruteforcerer/src/hooks/index.js
+++ resources/js/bruteforcerer/src/hooks/index.js
@@ -1,4 +1,3 @@
 import { usePhpBruteForce } from './usePhpBruteForce';
 import { useFetch, FETCH_METHODS } from './useFetch';
-import { useRegisterUsername } from './useRegisterUsername';
-export { usePhpBruteForce, useFetch, FETCH_METHODS, useRegisterUsername };
+export { usePhpBruteForce, useFetch, FETCH_METHODS };
